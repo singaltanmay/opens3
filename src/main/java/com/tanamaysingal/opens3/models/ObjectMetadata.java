7110e5f8879c444d338b0cf99391851428067848
Tanmay Singal <tanmaysingal@icloud.com>
2023-06-14 08:28:03 -0700

    mvnw

--- src/main/java/com/tanamaysingal/opens3/models/ObjectMetadata.java
+++ src/main/java/com/tanamaysingal/opens3/models/ObjectMetadata.java
@@ -1,6 +1,24 @@
 package com.tanamaysingal.opens3.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class ObjectMetadata {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
